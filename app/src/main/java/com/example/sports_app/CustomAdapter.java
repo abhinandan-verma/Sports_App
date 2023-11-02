4fd00076d54838b73bee55ed97019e18c8a6050f
abhinandan-verma <abhinandanverma551@gmail.com>
2023-11-02 19:35:31 +0530

    Added RecycleView & CardView

---
+++ app/src/main/java/com/example/sports_app/CustomAdapter.java
@@ -0,0 +1,65 @@
+package com.example.sports_app;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+//this Class is an adapter Class
+public class CustomAdapter
+        extends RecyclerView.Adapter<CustomAdapter.SportsViewHolder> {
+
+    public List<Sport> sportList;
+
+    public CustomAdapter(List<Sport> sportList) {
+        this.sportList = sportList;
+    }
+
+    @NonNull
+    @Override
+    public SportsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        //Inflating the Layout for each item in the recyclerView
+
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.card_item_layout,parent,false);
+
+        return new SportsViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SportsViewHolder holder, int position) {
+        //called for each item in the list and is responsible for
+        //bonding the data from the Sport Object to the Views
+        //within the 'SportViewHolder'
+
+        Sport sport = sportList.get(position);
+        holder.textView.setText(sport.sportName);
+        holder.imageView.setImageResource(sport.sportImg);
+    }
+
+    @Override
+    public int getItemCount() {
+        return sportList.size();
+    }
+
+    public  class SportsViewHolder
+            extends RecyclerView.ViewHolder{
+        //Holds the reference to the views within the item layout
+
+        TextView textView;
+        ImageView imageView;
+
+        public SportsViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            textView = itemView.findViewById(R.id.textView);
+            imageView = itemView.findViewById(R.id.imageView);
+        }
+    }
+}
--- app/src/main/java/com/example/sports_app/MainActivity.java
+++ app/src/main/java/com/example/sports_app/MainActivity.java
@@ -1,14 +1,50 @@
 package com.example.sports_app;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    //dataSource
+    private List<Sport> sportList;
+    //Adapter
+    private  CustomAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        sportList = new ArrayList<>();
+
+        Sport s1 = new Sport("FootBall",R.drawable.football);
+        Sport s2 = new Sport("Cricket",R.drawable.cricket);
+        Sport s3 = new Sport("BasketBall",R.drawable.basketball);
+        Sport s4 = new Sport("Tennis",R.drawable.tennis);
+        Sport s5 = new Sport("Volleyball",R.drawable.volleyball);
+        Sport s6 = new Sport("Kabaddi",R.drawable.kabaddi);
+        Sport s7 = new Sport("Badminton",R.drawable.badminton);
+        sportList.add(s1);
+        sportList.add(s2);
+        sportList.add(s3);
+        sportList.add(s4);
+        sportList.add(s5);
+        sportList.add(s6);
+        sportList.add(s7);
+
+        adapter = new CustomAdapter(sportList);
+
+        //LayoutManager - responsible for positioning & measuring the items in recyclerView
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
     }
 }
